821e25599a1489f10b6556525edac0afa6bf0359
Rakesh Patri <sodel@RAKESH.(none)>
2013-11-27 12:49:25 +0530

    Phase1-001 facebook integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Lazuli::Application.routes.draw do
-  devise_for :users
-  
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   root :to => "home#index"
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131126072117) do
+ActiveRecord::Schema.define(:version => 20131126134248) do
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(:version => 20131126072117) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
