0ba59eae832e1ab009e031353f2ceef3e4f16e13
Pravin Mhatre <sodel@Pravin-ubuntu.(none)>
2013-11-29 18:32:11 +0530

    User model refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,69 +5,15 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid ,:first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid , :name
   # attr_accessible :title, :body
 
-  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    if user
-      return user
-    else
-      registered_user = User.where(:email => auth.info.email).first
-      if registered_user
-        return registered_user
-      else
-        user = User.create!(first_name:auth.extra.raw_info.first_name,
-        										last_name:auth.extra.raw_info.last_name,
-                            provider:auth.provider,
-                            uid:auth.uid,
-                            email:auth.extra.raw_info.email,
+  def self.find_for_oauth(oauth_raw_data, oauth_user_data, signed_in_resource=nil )
+    return User.where("(provider = '#{oauth_raw_data.provider}' AND uid = '#{oauth_raw_data.uid}') OR email='#{oauth_user_data.email}'").first || User.create!(name:oauth_user_data.name,
+                            provider:oauth_raw_data.provider,
+                            uid:oauth_raw_data.uid,
+                            email:oauth_user_data.email,
                             password:Devise.friendly_token[0,20],
                           )
-      end
-    end
   end
-
-  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
-    data = access_token.info
-    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
-    if user
-      return user
-    else
-      registered_user = User.where(:email => access_token.info.email).first
-      if registered_user
-        return registered_user
-      else
-        user = User.create!(first_name: data["first_name"],
-        										last_name: data["last_name"],
-									          provider:access_token.provider,
-									          email: data["email"],
-									          uid: access_token.uid ,
-									          password: Devise.friendly_token[0,20],
-									        )
-      end
-    end
-  end
-
-  def self.find_for_linkedin(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    if user
-      return user
-    else
-      registered_user = User.where(:email => auth.info.email).first
-      if registered_user
-        return registered_user
-      else
-        user = User.create!(first_name:auth.info.first_name,
-        										last_name:auth.info.last_name,
-                            provider:auth.provider,
-                            uid:auth.uid,
-                            email:auth.info.email,
-                            password:Devise.friendly_token[0,20],
-                          )
-      end
-    end
-  end
-
 end
-
