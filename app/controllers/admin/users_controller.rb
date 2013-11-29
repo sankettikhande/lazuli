935394d1f22926389d0e3bdf861b04419bf7d8a7
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2013-11-29 19:17:53 +0530

    add user module added in admin pannel

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,19 @@ class Admin::UsersController < AdminController
   def index
     @users = User.all
   end
+
+  def new
+  	@user = User.new
+  end	
+
+  def create
+  	@user = User.new(params[:user])
+  	respond_to do |format|
+  		if @user.save
+  			format.html {redirect_to admin_users_url}
+  		else
+  			format.html { render :action => "new" }
+  		end	
+  	end	
+  end	
 end
