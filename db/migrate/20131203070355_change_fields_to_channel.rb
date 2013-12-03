06d3b8d7dd43c0779ae0676a0fbe270729bdae6d
Pravin Mhatre <sodel@Pravin-ubuntu.(none)>
2013-12-03 18:02:19 +0530

    Topic model, fixed migration

--- db/migrate/20131203070355_change_fields_to_channel.rb
+++ db/migrate/20131203070355_change_fields_to_channel.rb
@@ -2,7 +2,6 @@ class ChangeFieldsToChannel < ActiveRecord::Migration
   def change
   	add_column :channels, :company_contact_name, :string if !column_exists? :channels, :company_contact_name
   	add_column :channels, :company_postal_address, :string if !column_exists? :channels, :company_postal_address
-  	remove_column :channels, :company_contact_number, :string if column_exists? :channels, :company_contact_number
-  	add_attachment :channels, :image
+  	remove_column :channels, :company_contact_number if column_exists? :channels, :company_contact_number
   end
 end
