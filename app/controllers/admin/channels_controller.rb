85b74b7c02df9a71da8776b18a32253113827558
nileshpatil47 <nileshpatil47@yahoo.com>
2013-12-05 12:47:49 +0530

    channel course and course image added

--- app/controllers/admin/channels_controller.rb
+++ app/controllers/admin/channels_controller.rb
@@ -5,6 +5,7 @@ class Admin::ChannelsController < AdminController
 
   def new
   	@channel = Channel.new
+    @channel.courses.build(name: '')
   end	
 
   def create
@@ -20,6 +21,7 @@ class Admin::ChannelsController < AdminController
 
   def edit
     @channel = Channel.find(params[:id])
+    @channel.courses.build(name: '') if @channel.courses.empty?
   end
 
   def update
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,8 @@
 class Course < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :name, :description, :trainer_name, :trainer_biography
+  attr_accessible :name, :description, :trainer_name, :trainer_biography, :image
+
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :path => ":rails_root/public/images/courses/:id/:style/:basename.:extension"
 
   #ASSOCIATIONS
   has_many :channel_courses
@@ -8,8 +10,11 @@ class Course < ActiveRecord::Base
   has_many :topics
   has_many :course_permissions
 
+
   #VALIDATIONS
   validates :name, :presence => true
+  validates_attachment_size :image, :less_than => 3.megabytes
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png','image/gif','image/jpg']
 
   #SCOPES
 
