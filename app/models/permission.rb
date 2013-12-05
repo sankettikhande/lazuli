d4b0d199854a78102e13798b583a5fca2f39df72
Rakesh Patri <sodel@RAKESH.(none)>
2013-12-05 16:31:27 +0530

    deleted permission table and renamed columns in new permission tables

--- app/models/permission.rb
+++
@@ -1,7 +0,0 @@
-class Permission < ActiveRecord::Base
-  # attr_accessible :channel_id, :course_id, :create, :share, :user_id, :watch
-  belongs_to :user
-  belongs_to :channel
-  belongs_to :course
-  
-end
