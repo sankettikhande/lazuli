f061e5c9b941531edd2fd4b5840d5a8c27f39c53
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2013-12-13 11:31:25 +0530

    Multiple users form added

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -71,8 +71,42 @@ class Admin::UsersController < AdminController
     end  
   end
 
+  def new_bulk
+    @user =User.new
+    set_instance
+    @user_error = ''
+    @user.user_channel_subscriptions.build
+  end 
+
+  def create_bulk
+    @bulk_user_errors = User.bulksheet_errors(params[:user][:file])
+    if @bulk_user_errors.blank? 
+      @users = User.import_users(params[:user])
+      respond_to do |format|
+        if @users.map(&:valid?).all?
+          @users.each(&:save!)
+          format.html {redirect_to admin_users_url}
+        else
+          build_user_and_association(:errors => @users.map(&:errors).map(&:full_messages).flatten)
+          format.html { render :action => "new_bulk" }
+        end
+      end    
+    else
+      build_user_and_association(:errors => @bulk_user_errors)
+      render :action => "new_bulk"
+    end  
+  end
+
 private
 
+  def build_user_and_association(options={})
+    user_params = params[:user]
+    user_params.delete("file")
+    @user = User.new(user_params)
+    @bulk_user_errors = options[:errors] || [] 
+    set_instance
+  end
+  
   def set_instance
     @channels = Channel.all
     @subscriptions = Subscription.all
