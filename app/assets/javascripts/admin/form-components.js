8dd65b90103ef3a96d3812791faf301dba8fb267
nileshpatil47 <nileshpatil47@yahoo.com>
2013-12-16 12:08:57 +0530

    form component changes

--- app/assets/javascripts/admin/form-components.js
+++ app/assets/javascripts/admin/form-components.js
@@ -448,15 +448,15 @@ var FormComponents = function () {
         $("#mask_decimal").inputmask('decimal', {
             rightAlignNumerics: false
         }); //disables the right alignment of the decimal input
-        $("#mask_currency").inputmask('€‚¬ 999.999.999,99', {
+        $("#mask_currency").inputmask('€ 999.999.999,99', {
             numericInput: true
-        }); //123456  =>  €‚¬ ___.__1.234,56
+        }); //123456  =>  € ___.__1.234,56
 
-        $("#mask_currency2").inputmask('€‚¬ 999,999,999.99', {
+        $("#mask_currency2").inputmask('€ 999,999,999.99', {
             numericInput: true,
             rightAlignNumerics: false,
             greedy: false
-        }); //123456  =>  €‚¬ ___.__1.234,56
+        }); //123456  =>  € ___.__1.234,56
         $("#mask_ssn").inputmask("999-99-9999", {
             placeholder: " ",
             clearMaskOnLostFocus: true
