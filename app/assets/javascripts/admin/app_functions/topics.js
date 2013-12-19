8649951ed1c7b1c388271108e8222caf87eed997
nileshpatil47 <nileshpatil47@yahoo.com>
2013-12-19 15:55:36 +0530

    form related changes

--- app/assets/javascripts/admin/app_functions/topics.js
+++ app/assets/javascripts/admin/app_functions/topics.js
@@ -5,4 +5,15 @@ $(document).ready(function(){
 		var arguments = ['course','get_channel_info.json','admin/courses','topic_channel_id','t-channel','id','name']
 		getJsonDatas.apply(null,arguments)
 	})
+	videoAccordionForm();
+	$('.add_nested_fields').live('click',function(){
+		videoAccordionForm();
+	})
 })
+
+function videoAccordionForm(){
+	App.initUniform();
+	$(".video_tag_list").select2({
+      tags: []
+  });
+}
