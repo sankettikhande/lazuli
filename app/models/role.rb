e4d347d835a4907163e521820d438887026b0c7e
Pravin Mhatre <sodel@Pravin-ubuntu.(none)>
2013-12-23 15:23:29 +0530

    Adding default role assignment

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,11 @@
 class Role < ActiveRecord::Base
+
+  @@valid_role_names = ['admin', 'user']
+
   has_and_belongs_to_many :users, :join_table => :users_roles
   belongs_to :resource, :polymorphic => true
   
   scopify
+
+  validates :name, :inclusion => {:in => @@valid_role_names}
 end
