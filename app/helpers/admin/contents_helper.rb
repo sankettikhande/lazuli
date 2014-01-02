01a08534ec7642e8eb179ba0bdc3c1421804246a
nileshpatil47 <nileshpatil47@yahoo.com>
2014-01-02 17:00:47 +0530

    revert change

--- app/helpers/admin/contents_helper.rb
+++ app/helpers/admin/contents_helper.rb
@@ -1,9 +1,7 @@
 module Admin::ContentsHelper
 
 	def channel_name_for_topic(id)
-		if id
-		  channel = Channel.cached_find(id)
-		  channel_name = channel.name if channel
-		end
+	  channel = Channel.cached_find(id)
+	  channel_name = channel.name if channel
 	end
 end
