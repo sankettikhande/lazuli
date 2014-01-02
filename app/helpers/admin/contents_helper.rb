f669ce9a0f42609d9106bf41c36e11806e74bba0
nileshpatil47 <nileshpatil47@yahoo.com>
2014-01-02 15:42:57 +0530

    content helper check added

--- app/helpers/admin/contents_helper.rb
+++ app/helpers/admin/contents_helper.rb
@@ -1,7 +1,9 @@
 module Admin::ContentsHelper
 
 	def channel_name_for_topic(id)
-	  channel = Channel.cached_find(id)
-	  channel_name = channel.name if channel
+		if id
+		  channel = Channel.cached_find(id)
+		  channel_name = channel.name if channel
+		end
 	end
 end
