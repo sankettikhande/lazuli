4da0f133cf0ca3a1910abebd572622872aa44b3e
Prashant Dixit <prashant.dixit@sodelsolutions.com>
2014-01-08 11:31:26 +0530

    change migration name for subscription in course.

--- db/migrate/20140106140342_add_subscription_id_to_course.rb
+++
@@ -1,9 +0,0 @@
-class AddSubscriptionIdToCourse < ActiveRecord::Migration
-	def change
-		 	Course.all.each do |course|
-		 	 	if(course.subscription_ids.blank?)
-			 		course.destroy
-			 	end
-		 	end
-	end
-end
