48821800f7622e44bb4a23a523fcea0161c596df
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2014-01-08 10:44:40 +0530

    validation engine bind with form id

--- app/assets/javascripts/admin/app_functions/users.js
+++ app/assets/javascripts/admin/app_functions/users.js
@@ -1,3 +1,8 @@
 $(".user_action").click(function(){
   window.location = $("#user_mode:checked").data("redirect-url")
 })
+
+$("#user_form").validationEngine({
+	promptPosition : "bottomLeft",
+	autoPositionUpdate: true
+});
