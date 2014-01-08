7e784de9fa77c403e36330b5019bec0758ba2892
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2014-01-08 16:15:11 +0530

    remove course link added for user channel subscriptions

--- app/models/user_channel_subscription.rb
+++ app/models/user_channel_subscription.rb
@@ -1,12 +1,14 @@
 class UserChannelSubscription < ActiveRecord::Base
   attr_accessible :user_id, :channel_id, :subscription_id, :subscription_date, :expiry_date, :course_id, :permission_watch, :permission_create, :permission_share
+  attr_accessor :_destroy
+  attr_accessible :_destroy
   
   belongs_to :user
   belongs_to :subscription
   belongs_to :channel
   belongs_to :course
 
-  validates_presence_of :channel_id, :subscription_id, :subscription_date, :expiry_date, :course_id
+  #validates_presence_of :channel_id, :subscription_id, :subscription_date, :expiry_date, :course_id
   validates :user_id, :uniqueness => {:scope => [:channel_id, :course_id]}
   after_create :update_channel_user_count
 
