79a48fc666062669d9e73e52145ae851aed2b630
Pravin Mhatre <sodel@Pravin-ubuntu.(none)>
2014-01-08 10:53:10 +0530

    Updated precompile list

--- config/recipes/sphinx.rb
+++ config/recipes/sphinx.rb
@@ -3,21 +3,12 @@ namespace :sphinx do
   task :setup_ts_yml, :roles => :app do
     ts_config = <<-EOF
       #{rails_env.downcase}:
-          port: 3320
-          address: 127.0.0.1
           mem_limit: 256M
           morphology: stem_en
           enable_star: true
           min_infix_len: 0
           min_prefix_len: 1
-          searchd_file_path: "#{current_path}/sphinx/index/"
-          query_log_file: "#{current_path}/log/searchd.query.log"
-          searchd_log_file: "#{current_path}/log/searchd.log"
-          pid_file: "#{current_path}/tmp/pids/searchd.pid"
-          config_file: "#{current_path}/config/#{rails_env}.sphinx.conf"
-          bin_path: '/usr/local/sphinx/bin'
-
-    EOF
+      EOF
 
     run "mkdir -p #{shared_path}/config"
     put ts_config, "#{shared_path}/config/thinking_sphinx.yml"    
@@ -36,5 +27,6 @@ end
 before "sphinx:configure_and_index", "sphinx:symlink_ts_yml"
 before "sphinx:symlink_ts_yml", "thinking_sphinx:configure"
 after "sphinx:symlink_ts_yml", "thinking_sphinx:index"
+after "sphinx:setup_ts_yml", "sphinx:symlink_ts_yml"
 
 
