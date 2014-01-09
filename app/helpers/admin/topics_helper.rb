294db2e38313470c472f3687fc13bdc103ab179d
nileshpatil47 <nileshpatil47@yahoo.com>
2014-01-09 17:47:51 +0530

    action method changes

--- app/helpers/admin/topics_helper.rb
+++ app/helpers/admin/topics_helper.rb
@@ -8,6 +8,9 @@ module Admin::TopicsHelper
 	end
 
 	def topic_actions(topic)
-		"<a href='/admin/topics/#{topic.id}/edit' class='btn btn-trans' data-no-turbolink='true' rel='tooltip' title='Edit Topic'><i class='fa fa-edit'></i></a><a href='/admin/topics/#{topic.id}' class='btn btn-trans' data-method='delete' data-confirm='Are you sure you want to delete?' rel='tooltip' title='Delete Topic'><i class='fa fa-ban'></i></a><a href='/admin/topics/#{topic.id}?Publish=Publish' class='btn btn-trans' data-method='put' rel='tooltip nofollow' title='Publish Topic'><i class='fa fa-cloud-upload'></i></a>".html_safe
+		action_str = "<a href='/admin/topics/#{topic.id}/edit' class='btn btn-trans' data-no-turbolink='true' rel='tooltip' title='Edit Topic'><i class='fa fa-edit'></i></a>"
+		action_str << "<a href='/admin/topics/#{topic.id}' class='btn btn-trans' data-method='delete' data-confirm='Are you sure you want to delete?' rel='tooltip' title='Delete Topic'><i class='fa fa-ban'></i></a>"
+		action_str << link_to(raw_field('fa fa-cloud-upload'), "#{admin_topic_path(topic)}?Publish=Publish", :method => :put, :class => "btn btn-trans", :rel=>"tooltip", :title=>"Publish Topic" ) if !topic.published?
+		action_str.html_safe
 	end
 end
--- app/helpers/admin/videos_helper.rb
+++ app/helpers/admin/videos_helper.rb
@@ -5,6 +5,9 @@ module Admin::VideosHelper
 	end
 
 	def video_actions(video)
-    "<a href='/admin/videos/#{video.id}/edit' class='btn btn-trans' rel='tooltip' title='Edit Video'><i class='fa fa-edit'></i></a><a href='/admin/videos/#{video.id}' class='btn btn-trans' data-method='delete' data-confirm='Are you sure you want to delete?' rel='tooltip' title='Delete Video'><i class='fa fa-ban'></i></a><a href='/admin/videos/#{video.id}/upload' class='btn btn-trans' rel='tooltip' title='Publish Video'><i class='fa fa-cloud-upload'></i></a>".html_safe
+    action_str = "<a href='/admin/videos/#{video.id}/edit' class='btn btn-trans' rel='tooltip' title='Edit Video'><i class='fa fa-edit'></i></a>"
+    action_str << "<a href='/admin/videos/#{video.id}' class='btn btn-trans' data-method='delete' data-confirm='Are you sure you want to delete?' rel='tooltip' title='Delete Video'><i class='fa fa-ban'></i></a>"
+    action_str << link_to(raw_field('fa fa-cloud-upload'), upload_admin_video_path(video), :class => "btn btn-trans", :rel=>"tooltip", :title=>"Publish Video" ) if !video.published?
+    action_str.html_safe
   end
 end
