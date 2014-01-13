100eae2a7861ab2ccf91025a0e037d0c73374d8c
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2014-01-13 16:54:20 +0530

    class name changed

--- app/assets/javascripts/admin/app_functions/video_accordion.js
+++ app/assets/javascripts/admin/app_functions/video_accordion.js
@@ -1,15 +1,15 @@
 jQuery(function ($) {
-	if($('.seqNum').val() == '')
-		$('.seqNum').val(1)
+	if($('.sequence_number').val() == '')
+		$('.sequence_number').val(1)
 	$(document).on('nested:fieldAdded:videos', function(event) {
 		$("#accordion1").find(".fields").each(function(index){
-			$(this).find(".seqNum").val(index + 1)
+			$(this).find(".sequence_number").val(index + 1)
 		})
 	});
 	$(document).on('nested:fieldRemoved:videos', function(event) {
 		$(event.target).remove()
 		$("#accordion1").find(".fields").each(function(index){
-			$(this).find(".seqNum").val(index + 1)
+			$(this).find(".sequence_number").val(index + 1)
 		})
 	});
 });
