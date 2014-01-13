7acfe97328da048eeda30b643ba7b8f524fe9323
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2014-01-13 21:41:25 +0530

    alignment done

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,9 +1,9 @@
 module Admin::UsersHelper
   def user_actions(user)
   	if user.confirmed?
-    	"<a href='/admin/users/#{user.id}/edit' rel='tooltip' title='Edit User' data-no-turbolink='true' class='btn-trans'><i class='fa fa-edit'></i></a><a href='/admin/users/#{user.id}' data-method='delete' data-confirm='Are you sure you want to delete?' rel='tooltip' title='Delete User' class='btn-trans'><i class='fa fa-ban'></i></a>".html_safe
+  		"<a href='/admin/users/#{user.id}/edit' rel='tooltip' title='Edit User' data-no-turbolink='true' class='btn-trans'><i class='fa fa-edit'></i></a><a href='/admin/users/#{user.id}' data-method='delete' data-confirm='Are you sure you want to delete?' rel='tooltip' title='Delete User' class='btn-trans'><i class='fa fa-ban'></i></a>".html_safe
   	else
-    	"<a href='#' rel='tooltip' title='Edit User' data-no-turbolink='true' class='btn-trans'><i class='fa fa-edit'></i></a><a href='/admin/users/#{user.id}' data-method='delete' data-confirm='Are you sure you want to delete?' rel='tooltip' title='Delete User' class='btn-trans'><i class='fa fa-ban'></i></a>".html_safe
-    end
+  		"<a href='#' rel='tooltip' title='Edit User' data-no-turbolink='true' class='btn-trans'><i class='fa fa-edit'></i></a><a href='/admin/users/#{user.id}' data-method='delete' data-confirm='Are you sure you want to delete?' rel='tooltip' title='Delete User' class='btn-trans'><i class='fa fa-ban'></i></a>".html_safe
+  	end
   end
 end
