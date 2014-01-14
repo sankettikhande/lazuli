56fa341fdfc07fdca8540c65ac68597148001492
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2014-01-14 17:24:10 +0530

    css changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
       errors_ul = content_tag(:ul)  do
         error_messages.collect {|error_msg| concat(content_tag(:li, error_msg))}
       end
-      content_tag(:div, errors_ul, :class => "small_alert error devise_errors", :style=>'color:red;')
+      content_tag(:div, errors_ul, :class => "small_alert error devise_errors", :style=>'color: #FF0000;')
     end
   end
 
