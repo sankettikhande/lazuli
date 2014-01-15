6e7299d240c08217b552016589bcd7f4174b2062
nileshpatil47 <nileshpatil47@yahoo.com>
2014-01-15 20:17:46 +0530

    bookmark accordion change

--- app/controllers/admin/bookmarks_controller.rb
+++ app/controllers/admin/bookmarks_controller.rb
@@ -12,4 +12,13 @@ class Admin::BookmarksController < AdminController
 		@video = Video.find(params[:id])
 		@video.bookmarks.build if @video.bookmarks.blank?
 	end
+
+	def sort_bookmarks(params)
+		bookmarks_attributes = ActiveSupport::HashWithIndifferentAccess.new
+		bookmarks_arr = params[:video][:bookmarks_attributes].map{ |x,y| y }.sort {|a,b| a['time']<=>b['time']}
+		bookmarks_arr.each_with_index do |bookmark, index|
+			params[:video][:bookmarks_attributes][index.to_s] = bookmark
+		end
+		ActiveSupport::HashWithIndifferentAccess.new(params)
+	end
 end
