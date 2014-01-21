4f6fc6427c5729b46943f56f37cd020ab87ef625
nileshpatil47 <nileshpatil47@yahoo.com>
2014-01-21 15:28:51 +0530

    new videos landing page changes

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,12 @@ module HomeHelper
 	def is_active_videos index
 		index.zero? ? "active" : nil
 	end
+
+	def thumbnail_image(video)
+		if video.thumbnail_data
+      video.get_best_thumbnail
+    else
+     video.image.present? ? video.image.url : "http://b.vimeocdn.com/thumbnails/defaults/default.480x640.jpg"
+    end
+  end
 end
