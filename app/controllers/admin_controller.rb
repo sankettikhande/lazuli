5402ea9605ea4374f0c693f388901df074dfa638
Rakesh Patri <sodel@RAKESH.(none)>
2014-01-23 16:28:14 +0530

    rake task added to modify db

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,20 +1,22 @@
 class AdminController < ApplicationController
-  before_filter :verify_admin
+  before_filter :verify_admin, :get_user_roles
   layout 'admin'
 
   def index
-    user_roles = current_user.roles.map(&:name)
-    if user_roles.include? "admin" or user_roles.include? "channel_admin"
+    if @user_roles.include? "admin" or @user_roles.include? "channel_admin"
       redirect_to admin_users_path
-    elsif user_roles.include? "course_admin"
+    elsif @user_roles.include? "course_admin"
       redirect_to admin_contents_path
     end
   end
 
   private
+  def get_user_roles
+    @user_roles = current_user.roles.map(&:name)
+  end  
 
   def verify_admin
-    user_roles = current_user.roles.map(&:name)
-    redirect_to root_url if !(user_roles.include? "admin" or user_roles.include? "channel_admin" or user_roles.include? "course_admin")
+    get_user_roles
+    redirect_to root_url if !(@user_roles.include? "admin" or @user_roles.include? "channel_admin" or @user_roles.include? "course_admin")
   end
 end
