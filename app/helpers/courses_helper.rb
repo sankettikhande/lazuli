9f3108fd0bb2d170349816090b1a0aba191fc0e1
nileshpatil47 <nileshpatil47@yahoo.com>
2014-01-23 19:59:46 +0530

    space changes

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -11,15 +11,15 @@ module CoursesHelper
 		if @video
 			@video.topic.id == topic.id ? "active panel" : "panel"
 		else
-    	index.zero? ? "active panel" : "panel"
-    end
-  end
+  		index.zero? ? "active panel" : "panel"
+		end
+	end
 
-  def is_accordion topic, topic_index
-    if @video
+	def is_accordion topic, topic_index
+		if @video
 			"in" if @video.topic.id == topic.id
 		else
-    	"in" if topic_index.zero?
-    end
-  end
+			"in" if topic_index.zero?
+		end
+	end
 end
