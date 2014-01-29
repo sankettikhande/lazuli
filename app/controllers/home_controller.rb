fab649080ffb1d9cf9f5204c695592ab6c15af3b
nileshpatil47 <nileshpatil47@yahoo.com>
2014-01-29 11:27:57 +0530

    In groups home page changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 	skip_before_filter :authenticate_user!
   def index
   	if user_signed_in?
-  		@videos = Video.published.order('created_at').limit(Settings.data_count.latest_video)
+  		@videos = Video.published.order('created_at').limit(Settings.data_count.latest_video).in_groups_of(Settings.data_count.latest_video_frame, false)
   		@courses = Course.last(Settings.data_count.courses)
   	else
   		@courses = Course.last(3)
