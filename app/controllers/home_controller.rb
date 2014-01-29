2ed4516a879113b4d30fa15f920a962bdb2df000
nileshpatil47 <nileshpatil47@yahoo.com>
2014-01-29 12:03:56 +0530

    remove skip before filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
-	skip_before_filter :authenticate_user!
-  def index
+	def index
   	if user_signed_in?
   		@videos = Video.published.order('created_at').limit(Settings.data_count.latest_video).in_groups_of(Settings.data_count.latest_video_frame, false)
   		@courses = Course.last(Settings.data_count.courses)
