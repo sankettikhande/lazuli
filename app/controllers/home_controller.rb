3314d1b1b7aea3083150bb1d118b5e75835c2581
nileshpatil47 <nileshpatil47@yahoo.com>
2014-02-04 13:59:35 +0530

    home carousel changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,8 @@ class HomeController < ApplicationController
       @videos = Video.cached_scope('published', {:limit => Settings.data_count.latest_video }).in_groups_of(Settings.data_count.latest_video_frame, false)
   		@courses = Course.last(Settings.data_count.courses)
   	else
-  		@courses = Course.last(Settings.data_count.lending_page_courses)
+      @topics = Topic.cached_scope('published', {:limit => 6 }).in_groups_of(2, false)
+      @courses = Course.last(Settings.data_count.lending_page_courses)
   		respond_to do |format|
 	      format.html{ render 'devise/sessions/new', :layout => 'devise' }
 	    end
