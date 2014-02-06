cef2a6f7358d905806ae2a789a627a92458cdd12
nileshpatil47 <nileshpatil47@yahoo.com>
2014-02-06 16:15:05 +0530

    video wtach list changes

--- app/controllers/watch_lists_controller.rb
+++ app/controllers/watch_lists_controller.rb
@@ -1,6 +1,6 @@
 class WatchListsController < ApplicationController
 	before_filter :authenticate_user!
 	def index
-		@watch_list = current_user.watch_lists
+		@videos = Video.cached_find(current_user.watch_lists.map(&:video_id))
 	end
 end
