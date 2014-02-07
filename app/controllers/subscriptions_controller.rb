1dc96a1bbd0f473f9df37870b8dc84173bb651a9
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2014-02-07 15:48:09 +0530

    subscription issue fixed

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,10 +2,12 @@ class SubscriptionsController < ApplicationController
 	before_filter :authenticate_user!
 
 	def subscribe_course
-		@subscription = UserChannelSubscription.new(params[:course_subscription])
+		subscription_param = params[:course_subscription].except('subscription_id')
+		@user_channel_subscription = UserChannelSubscription.where(subscription_param).first || UserChannelSubscription.new(params[:course_subscription])
 		sid = params[:course_subscription][:subscription_id]
 		duration = "duration_#{sid}".to_sym
-		@subscription.set_subscription_date_range(params[duration].to_i)
-		@subscription.save
+		@user_channel_subscription.subscription_id = sid
+		@user_channel_subscription.set_subscription_date_range(params[duration].to_i)
+		@user_channel_subscription.save
 	end
 end
