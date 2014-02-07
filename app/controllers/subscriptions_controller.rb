eb9f3c2badc3bbe4d2b929472fcda141aaeaa0f5
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2014-02-07 20:13:48 +0530

    public courses displayed in dropdowm

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,9 +4,8 @@ class SubscriptionsController < ApplicationController
 	def subscribe_course
 		subscription_param = params[:course_subscription].except('subscription_id')
 		@user_channel_subscription = UserChannelSubscription.where(subscription_param).first || UserChannelSubscription.new(params[:course_subscription])
-		sid = params[:course_subscription][:subscription_id]
-		duration = "duration_#{sid}".to_sym
-		@user_channel_subscription.subscription_id = sid
+		@user_channel_subscription.subscription_id = params[:course_subscription][:subscription_id]
+		duration = "duration_#{@user_channel_subscription.subscription_id}".to_sym
 		@user_channel_subscription.set_subscription_date_range(params[duration].to_i)
 		@user_channel_subscription.save
 	end
