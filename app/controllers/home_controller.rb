6380ebe5da157424f370252a33ed778469488c90
nileshpatil47 <nileshpatil47@yahoo.com>
2014-02-12 11:53:59 +0530

    html changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      @videos = Video.cached_scope('published', {:limit => Settings.data_count.latest_video })#.in_groups_of(Settings.data_count.latest_video_frame, false)
+      @videos = Video.cached_scope('published', {:limit => Settings.data_count.latest_video })
       @subscribed_courses = UserChannelSubscription.user_subscribed_courses(current_user)
       @courses = Course.public_channel_courses if @subscribed_courses.blank?
     else
-      @topics = Topic.cached_scope('published', {:limit => 6 }).in_groups_of(2, false)
+      @topics = Topic.published.where(:is_bookmark_video => false).limit(3)
       @courses = Course.public_channel_courses
       respond_to do |format|
         format.html{ render 'devise/sessions/new', :layout => 'devise' }
