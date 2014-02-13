42405c4e20d83028eff5568ac0f91e145832d7a8
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2014-02-13 16:32:42 +0530

    commented code removed

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,15 +2,7 @@ class ConfirmationsController < Devise::ConfirmationsController
 
   def create
     self.resource = resource_class.send_confirmation_instructions(params[resource_name])
-    respond_to do |format|
-      if successfully_sent?(resource)
-      	format.js
-      	# respond_with({}, :location => after_resending_confirmation_instructions_path_for(resource_name))
-	    else
-	      format.js
-	      # respond_with(resource)
-	    end
-    end
+    successfully_sent?(resource)
   end
 
 end
--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -3,14 +3,6 @@ class PasswordsController < Devise::PasswordsController
   def create
     self.resource = resource_class.send_reset_password_instructions(resource_params)
     yield resource if block_given?
-    respond_to do |format|
-      if successfully_sent?(resource)
-      	format.js
-      	# respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))
-	    else
-	      format.js
-	      # respond_with(resource)
-	    end
-    end
+    successfully_sent?(resource)
   end
 end  
