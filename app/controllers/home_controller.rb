424d0c5ddac4067c1d24b85f7f453a47366e1479
nileshpatil47 <nileshpatil47@yahoo.com>
2014-02-14 18:43:16 +0530

    all courses changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     if user_signed_in?
       @videos = Video.cached_scope('published', {:limit => Settings.data_count.latest_video })
       @subscribed_courses = UserChannelSubscription.user_subscribed_courses(current_user)
-      @courses = Course.public_channel_courses if @subscribed_courses.blank?
+      @courses = Course.all_public_channel_courses.take(10) if @subscribed_courses.blank?
     else
       @topics =  Topic.published.not_bookmarked.limit(3)
       @courses = Course.public_channel_courses
