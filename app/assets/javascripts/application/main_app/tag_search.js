d66036436ccc249fbe042cbfc3e056eb8b2e97be
nileshpatil47 <nileshpatil47@yahoo.com>
2014-02-27 18:09:26 +0530

    tag search added

---
+++ app/assets/javascripts/application/main_app/tag_search.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+	/* triiger search on key press of search input box */
+	$("#search_tags").unbind().keyup(function(){
+		var search_key = $(this).val();
+		if(search_key.length == 0 || search_key.length > 2){
+			get_search(search_key, "videos");
+		}
+	});
+
+	/* get values of search key & column name and fires ajax call to search records */
+	function get_search(search_key, controller_name){
+		$.ajax({
+			type: "GET",
+			url: "/"+controller_name+"/tag_search",
+			data: {sSearch: search_key, sSearch1: "tags", iDisplayStart: 15}
+		});
+	}
+});
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,13 +1,25 @@
 class VideosController < ApplicationController
 	before_filter :authenticate_user!
+	before_filter :load_data, :only => [:tag_videos, :tag_search]
 
 	def tag_videos
-		@videos = Video.search(:conditions => sphinx_condition(params[:search]))
+		@videos = Video.sphinx_search(params, current_user)
+		respond_to do |format|
+			format.html {}
+			format.js { render 'videos/tag_search'}
+		end
+	end
+
+	def tag_search
+		@videos = Video.sphinx_search(params, current_user)
 	end
 
 	private
 	
-	def sphinx_condition(tag)
-		tag.blank? ? {:status => 'Published'} : { :tags => "#{tag}*", :status => 'Published' }
+	def load_data
+		page = (params[:page] || 1).to_i - 1
+		params[:sSearch] = (params[:sSearch] || params[:search])
+		params[:iDisplayLength] = 10
+		params[:iDisplayStart] = params[:iDisplayLength] * page
 	end
 end
