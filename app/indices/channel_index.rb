54fedfe667388983c8dad66dd937925953a21267
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2014-02-28 11:20:16 +0530

    blank courses and topics issue resolved

--- app/indices/channel_index.rb
+++ app/indices/channel_index.rb
@@ -2,6 +2,7 @@ ThinkingSphinx::Index.define :channel, :with => :active_record, :delta => Thinki
   indexes name, :sortable => true
   indexes admin.name, :as => :channel_admin_user, :sortable => true
   indexes channel_type
+  indexes courses.topics.status, :as => :topic_status
   has created_at, updated_at, user_count, course_count
   has :id, :as => :channel_id
 end
