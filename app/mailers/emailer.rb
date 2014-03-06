eaea61d3bbff5269f1b1b2af4b55dfec0f1624af
Pravin Mhatre <sodel@Pravin-ubuntu.(none)>
2014-03-06 12:27:20 +0530

    Email addreses to settings

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -1,10 +1,11 @@
 class Emailer < ActionMailer::Base
-  default :from => "no-reply@lazuli.com"
+  default :from => Settings.email_settings.default_from_email
+
   def sent_contact_us(contact_us)  	
     @sent_on = Time.now
     @subject = contact_us.subject
     @contact_us = contact_us    
-    mail(:to => admin@lazuli.com , :subject => @subject, :content_type => "text/html")
+    mail(:to => Settings.email_settings.contact_us_recipients, :subject => @subject, :content_type => "text/html")
   end
 end
 
