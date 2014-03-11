4a7d923427ccf158e64d69674dde80dcbaa4eeea
Rakesh Patri <sodel@RAKESH.(none)>
2014-03-11 17:40:20 +0530

    channel admin + delete user button disabled as per permission

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,9 +1,12 @@
 module Admin::UsersHelper
-  def user_actions(user)
-  	if user.confirmed?
-  		link_to(link_raw_field('fa fa-edit'), edit_admin_user_path(user), :rel=>'tooltip', :title => 'Edit User', :class=>'btn-trans', :data =>{'no-turbolink'=>'true'}) << link_to(link_raw_field('fa fa-ban'), admin_user_path(user), :rel=>'tooltip', :title => 'Delete User', :class=>'btn-trans', :data =>{'no-turbolink'=>'true', :method => 'delete', :confirm => 'Are you sure you want to delete?'})
-  	else
-  		link_to(link_raw_field('fa fa-edit'), '#', :disabled => true, :rel=>'tooltip', :title => 'Edit User', :class=>'btn-trans', :data =>{'no-turbolink'=>'true'}) << link_to(link_raw_field('fa fa-ban'), admin_user_path(user), :rel=>'tooltip', :title => 'Delete User', :class=>'btn-trans', :data =>{'no-turbolink'=>'true', :method => 'delete', :confirm => 'Are you sure you want to delete?'})
-  	end
-  end
+	def user_actions(user)
+		if can? :destroy , user
+			link = link_to(link_raw_field('fa fa-ban'), admin_user_path(user), :rel=>'tooltip', :title => 'Delete User', :class=>'btn-trans', :data =>{'no-turbolink'=>'true', :method => 'delete', :confirm => 'Are you sure you want to delete?'})
+		end
+		if user.confirmed?
+			link_to(link_raw_field('fa fa-edit'), edit_admin_user_path(user), :rel=>'tooltip', :title => 'Edit User', :class=>'btn-trans', :data =>{'no-turbolink'=>'true'}) << link
+		else
+			link_to(link_raw_field('fa fa-edit'), '#', :disabled => true, :rel=>'tooltip', :title => 'Edit User', :class=>'btn-trans', :data =>{'no-turbolink'=>'true'}) << link
+		end
+	end
 end
