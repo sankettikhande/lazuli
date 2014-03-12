8b5461d67211b122385ed5eff745e74255305710
Rakesh <rakesh@rakesh-HP-ProBook-4410s.(none)>
2014-03-13 00:16:51 +0530

    modified query

--- app/models/user.rb
+++ app/models/user.rb
@@ -132,7 +132,7 @@ class User < ActiveRecord::Base
     if self.is_admin?
       Course.all
     else
-      Course.joins(:user_channel_subscriptions).where("channel_admin_user_id =? OR course_admin_user_id = ? OR user_channel_subscriptions.user_id = ?", self.id, self.id, self.id).group(:id)
+      Course.joins('LEFT OUTER JOIN user_channel_subscriptions ON user_channel_subscriptions.course_id = courses.id').where("channel_admin_user_id =? OR course_admin_user_id = ? OR user_channel_subscriptions.user_id = ?", self.id, self.id, self.id).group(:id)
     end
   end
 
