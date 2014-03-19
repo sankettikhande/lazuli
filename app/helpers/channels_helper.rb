a8adf8ceb77db9dbc081b6de0a1e2f800eebc65d
Prashant Dixit <prashant.dixit@sodelsolutions.com>
2014-03-19 21:17:14 +0530

    add subscribe button in browse library

--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -2,4 +2,12 @@ module ChannelsHelper
 	def add_image_title(objekt)
 		objekt.image.present? ? "Change Logo" : "Add Logo"
 	end
+
+  def browse_course_subscriptions course
+    if course.subscriptions.map(&:name).include?("Yearly Subscription")
+      subscriptions_id = course.subscriptions.where(:name => "Yearly Subscription").last.id
+      course_price = number_to_currency(course.course_subscriptions.where(subscription_id: subscriptions_id).last.subscription_price)
+      content_tag(:li, "1 Yr Subscriptions: #{course_price}")
+    end
+  end
 end
