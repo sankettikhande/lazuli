2e3272dd3fc27b24cab26464b5482f221803d5f5
Prashant Dixit <prashant.dixit@sodelsolutions.com>
2014-03-26 17:11:33 +0530

    Fixed Loader functionality in course and channel.

--- app/assets/javascripts/admin/app_functions/jquery_validation.js
+++ app/assets/javascripts/admin/app_functions/jquery_validation.js
@@ -1,12 +1,16 @@
-$(".formClass").validationEngine({
-	validateNonVisibleFields: true,
-	promptPosition : "bottomLeft",
-	autoPositionUpdate: true,
-	onValidationComplete: function(form, status) {
-		$('.formError').css("left","15px")
-		if(status == true){
-			$(window).spin();
-			return true
-		}
-	}
+$(window).load(function(){
+  $(".formClass").validationEngine({
+    validateNonVisibleFields: true,
+    promptPosition : "bottomLeft",
+    autoPositionUpdate: true,
+    onValidationComplete: function(form, status) {
+      $('.formError').css("left","15px")
+      console.log(status)
+      console.log("!!!!!!!!!!!!")
+      if(status == true){
+        $(window).spin();
+        return true
+      }
+    }
+  });
 });
