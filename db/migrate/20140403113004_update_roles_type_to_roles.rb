00a4a2654f6cdb18958f45b449758bd72e364ca1
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2014-04-03 17:29:24 +0530

    changes for default role

--- db/migrate/20140403113004_update_roles_type_to_roles.rb
+++
@@ -1,9 +0,0 @@
-class UpdateRolesTypeToRoles < ActiveRecord::Migration
-  def up
-  	Role.create(:name => 'channel_admin') if Role.find_by_name('channel_admin').blank?
-  	Role.create(:name => 'course_admin') if Role.find_by_name('course_admin').blank?
-  end
-
-  def down
-  end
-end
