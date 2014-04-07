207f4987b55aeb57916685df732e9d9be1aa9661
Rakesh Patri <sodel@RAKESH.(none)>
2014-04-07 15:25:09 +0530

    final entry for multiple workers + queues

--- config/recipes/delayed_job.rb
+++ config/recipes/delayed_job.rb
@@ -8,11 +8,9 @@
 # Target server roles are taken from delayed_job_args_per_role keys.
 namespace :delayed_job do
 
-  def args_per_host(host)
-    roles.each do |role|
-      find_servers(:roles => role).each do |server|
-        return args[role] if server.host == host
-      end
+  def args_per_host_role(host,role)
+    find_servers(:roles => role).each do |server|
+      return args[role] if server.host == host
     end
   end
 
@@ -26,15 +24,19 @@ namespace :delayed_job do
 
   desc "Start the delayed_job process"
   task :start, :roles => lambda { roles } do
-    find_servers_for_task(current_task).each do |server|
-      run "cd #{current_path};#{rails_env} script/delayed_job #{args_per_host server.host} start", :hosts => server.host
+    roles.each do |role|
+      find_servers_for_task(current_task).each do |server|
+        run "cd #{current_path};#{rails_env} script/delayed_job #{args_per_host_role server.host,role} start", :hosts => server.host
+      end
     end
   end
 
   desc "Restart the delayed_job process"
   task :restart, :roles => lambda { roles } do
-    find_servers_for_task(current_task).each do |server|
-      run "cd #{current_path};#{rails_env} script/delayed_job #{args_per_host server.host} restart", :hosts => server.host
+    roles.each do |role|
+      find_servers_for_task(current_task).each do |server|
+        run "cd #{current_path};#{rails_env} script/delayed_job #{args_per_host_role server.host,role} restart", :hosts => server.host
+      end
     end
   end
 end
